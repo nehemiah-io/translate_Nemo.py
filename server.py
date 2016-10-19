0f7a8662721fb2914a115eadf8f12b2c29bbab1c
Nehemiah Odior <odior@Mamo.local>
2016-10-19 15:10:39 -0700

    server testing/implementation updated with multiple routes

--- server.py
+++ server.py
@@ -3,17 +3,22 @@ from flask import Flask, request, jsonify, json
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def hello():
-#     return "Nehemiah has the best body."
+@app.route("/")
+def hello():
+    return "Dogs will rule the world soon!"
+
 @app.route("/pets", methods = ['GET', 'POST'])
 def storePets():
-    result = request.form
-    return result["age"]
+    json = request.get_json()
+    print(json['age'])
+    return jsonify({"theAge" : json['age']})
 
 @app.route("/hello", methods = ['GET', "POST"])
 def bigbutts():
-    result = request.form
-    return result["HELLO WORLD"]
+    return "HELLO Children!, Hi Chef!"
+
+@app.route("/hello/<name>")
+def guchimayne(name):
+    return "Hello " + name
 if __name__ == "__main__":
     app.run()
